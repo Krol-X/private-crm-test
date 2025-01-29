04d85a8bfbcc3fcad33a241eaec4414db90b401d
Alex Krol <krolmail@list.ru>
2025-01-29 18:41:11 +0600

    add order controller and service implementations

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,23 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\Services\OrdersServiceInterface;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
+use Inertia\Inertia;
 
 class OrdersController extends Controller
 {
+    private OrdersServiceInterface $orders;
+
+    function __construct(OrdersServiceInterface $orders_service)
+    {
+        $this->orders = $orders_service;
+    }
+
     function index(Request $request)
     {
-        return Response::json();
+        return Inertia::render('Orders', [
+            'orders' => $this->orders->getOrders(),
+        ]);
     }
 
     function store(Request $request)
     {
-        return Response::json();
+        $data = Validator::make($request->all(), [
+            'client_name' => 'string|required',
+            'client_phone' => 'string|size:11|required',
+            'tariff_id' => 'integer|required|exists:tariffs,id',
+            'schedule_type' => 'in:EVERY_DAY,EVERY_OTHER_DAY,EVERY_OTHER_DAY_TWICE|required',
+            'comment' => 'string|required',
+            'first_date' => 'date|required',
+            'last_date' => 'date|required',
+        ]);
+
+        if ($data->fails()) {
+            return response()->json(['error' => $data->errors()], 400);
+        }
+
+        $order = $this->orders->addOrder($data->validated());
+        return Response::json($order);
     }
 
-    function show(Request $request)
+    function show(Request $request, $order_id)
     {
-        return Response::json();
+        $order = $this->orders->getOrder($order_id);
+
+        if ($order !== null) {
+            return Response::json([
+                'data' => $order,
+            ]);
+        } else {
+            return Response::json(
+                ['error' => 'The requested order was not found'], 404
+            );
+        }
     }
 }
--- app/Services/OrdersService.php
+++ app/Services/OrdersService.php
@@ -3,21 +3,35 @@
 namespace App\Services;
 
 use App\Interfaces\Services\OrdersServiceInterface;
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Date;
 
 class OrdersService implements OrdersServiceInterface
 {
-    public function getOrders()
+    /**
+     * @return Collection<Order>
+     */
+    public function getOrders(): Collection
     {
-        // todo
+        $orders = Order::all();
+
+        return $orders;
     }
 
     public function getOrder(int $id)
     {
-        // todo
+        $order = Order::find($id);
+
+        return $order;
     }
 
     public function addOrder(array $fields)
     {
-        // todo
+        $order = new Order($fields);
+        $order["created_at"] = Date::now();
+        $order->save();
+
+        return $order;
     }
 }
